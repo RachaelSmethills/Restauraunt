083457cba0f8a0407fe2ddc954ef629bf8b7d725
Rachael Ann Smethills <RSI46@ukc02zmeh9md6m.home>
2020-10-13 16:18:04 +0100

    added joins to SQL

--- SQL/testscript.sql
+++ SQL/testscript.sql
@@ -3,6 +3,22 @@ CREATE TABLE restauraunts(
     name Text
 );
 
+CREATE TABLE menus(
+    id INTEGER PRIMARY KEY, 
+    name Text,
+    restaurauntId INTEGER,
+    FOREIGN KEY(restaurauntId) REFERENCES restauraunts(id)
+);
+
+CREATE TABLE items(
+    id INTEGER PRIMARY KEY, 
+    name Text,
+    price DECIMAL,
+    menuId INTEGER,
+    FOREIGN KEY(menuId) REFERENCES menus(id)
+);
+
+
 INSERT INTO restauraunts 
     (name) 
 VALUES  
@@ -10,14 +26,32 @@ VALUES
     ('Gildersome Arms'),
     ('The Unicorn');
 
+INSERT INTO menus 
+    (name, restaurauntId) 
+VALUES  
+    ('A La Carte', 1),
+    ('Lunch', 2),
+    ('Bar', 3);
+
+INSERT INTO items 
+    (name, price, menuId) 
+VALUES  
+    ('Pancakes', 6.65, 1),
+    ('Strawberry Parfait', 4.35, 2),
+    ('Tarte aux Citrone', 5.25, 3);
+
 UPDATE restauraunts 
 SET name = 'Call Lane Social' 
 WHERE id=1;
 
-SELECT * FROM restauraunts;
 
-DELETE FROM restauraunts
-WHERE id = 2;
+SELECT 
+    r.name as [restaurant name],
+    m.name as [menu],
+    i.NAME as item,
+    i.price
+FROM restauraunts r
+INNER JOIN menus m ON r.id = m.restaurauntId
+INNER JOIN Items i on i.menuId = m.Id;
 
-SELECT * FROM restauraunts;
 
